f2b8ddc50a4d50064c7c72e82fde69ccc696fe07
Emma Puls <epuls@atlassian.com>
2024-09-28 13:56:22 +0800

    Initial Commit

---
+++ moodTerm/moodTermApp.swift
@@ -0,0 +1,17 @@
+//
+//  moodTermApp.swift
+//  moodTerm
+//
+//  Created by Emma Puls on 28/9/2024.
+//
+
+import SwiftUI
+
+@main
+struct moodTermApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ moodTermTests/moodTermTests.swift
@@ -0,0 +1,17 @@
+//
+//  moodTermTests.swift
+//  moodTermTests
+//
+//  Created by Emma Puls on 28/9/2024.
+//
+
+import Testing
+@testable import moodTerm
+
+struct moodTermTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
