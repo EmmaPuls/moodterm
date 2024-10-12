ad33524281ac1ad6475f66c8a61dee84f24bbb20
EmmaPuls <got2love@gmail.com>
2024-10-12 22:19:16 +0800

    User interface created (#1)
    * Updated README
    
    * User Interface created
    
    * Fixed README
    
    ---------
    
    Co-authored-by: Emma Puls <emmapuls@Emmas-Air.localdomain>

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-extern crate nix;
+use cacao::appkit::window::Window;
+use cacao::appkit::{App, AppDelegate};
 use nix::pty::ForkptyResult;
 use nix::sys::termios::{tcgetattr, tcsetattr, SetArg, Termios};
 use nix::unistd::{execvp, read, write};
@@ -33,9 +34,22 @@ fn spawn_shell() -> BorrowedFd<'static> {
     }
 }
 
-fn main() {
+#[derive(Default)]
+struct BasicApp {
+    window: Window,
+}
+
+impl AppDelegate for BasicApp {
+    fn did_finish_launching(&self) {
+        self.window.set_title("Hello, world!");
+        self.window.set_minimum_content_size(400.0, 400.0);
+        self.window.set_titlebar_appears_transparent(true);
+        self.window.show();
+    }
+}
+
+fn start_terminal_emulation() {
     let stdin_fd = unsafe { BorrowedFd::borrow_raw(0) }; // File descriptor for stdin
-    // let stdout_fd = BorrowedFd::borrow_raw(1); // File descriptor for stdout
 
     // Set terminal to raw mode
     let original_termios = set_raw_mode(stdin_fd);
@@ -44,7 +58,8 @@ fn main() {
     let master_fd = spawn_shell();
 
     // Create a thread to read from the shell and write to stdout
-    let master_fd_clone = master_fd;
+    // Cloning the master_fd allows us to use it concurrently via the original and the clone.
+    let master_fd_clone = master_fd.clone();
     std::thread::spawn(move || {
         let mut buffer = [0u8; 1024];
         loop {
@@ -74,4 +89,14 @@ fn main() {
 
     // Restore terminal settings
     restore_terminal(stdin_fd, &original_termios);
+}
+
+fn main() {
+    // Start the terminal emulation in a separate thread
+    std::thread::spawn(move || {
+        start_terminal_emulation();
+    });
+
+    // Start the UI
+    App::new("com.moodterm.ui", BasicApp::default()).run();
 }
