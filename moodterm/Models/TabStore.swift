8667c124443a78a17b1fdcec72118da0b7267d87
Emma Puls <got2love@gmail.com>
2024-11-24 17:44:41 +0800

    Refactor application storage

---
+++ moodterm/Models/TabStore.swift
@@ -0,0 +1,83 @@
+import Combine
+import SwiftUI
+
+/// A class manages the state of tabs in the application.
+class TabStore: ObservableObject {
+    @Published var tabs: [Tab] = []
+    @Published var selectedTab: UUID?
+    var cancellables = Set<AnyCancellable>()
+
+    private var applicationSupportDirectory: URL {
+        let directory = FileManager.default.urls(for: .applicationSupportDirectory, in: .userDomainMask).first!
+        let moodtermDirectory = directory.appendingPathComponent("moodterm")
+        
+        // Ensure the moodterm directory exists
+        if !FileManager.default.fileExists(atPath: moodtermDirectory.path) {
+            do {
+                try FileManager.default.createDirectory(at: moodtermDirectory, withIntermediateDirectories: true, attributes: nil)
+            } catch {
+                print("Failed to create moodterm directory: \(error)")
+            }
+        }
+        
+        return moodtermDirectory
+    }
+
+    private var filename = "tabs.json"
+
+    private var databaseFileUrl: URL {
+        applicationSupportDirectory.appendingPathComponent(filename)
+    }
+
+    private func loadTabs(from storeFileData: Data) -> [Tab] {
+        do {
+            let decoder = JSONDecoder()
+            return try decoder.decode([Tab].self, from: storeFileData)
+        } catch {
+            print(error)
+            return [Tab(title: "Tab 1", viewModel: TerminalViewModel())]
+        }
+    }
+
+    init() {
+        print("Database file path: \(databaseFileUrl.path)") // Log the full path
+
+        if let data = FileManager.default.contents(atPath: databaseFileUrl.path) {
+            tabs = loadTabs(from: data)
+        } else {
+            if let bundledDatabaseUrl = Bundle.main.url(forResource: "tabs", withExtension: "json") {
+                if let data = FileManager.default.contents(atPath: bundledDatabaseUrl.path) {
+                    tabs = loadTabs(from: data)
+                }
+            } else {
+                tabs = [Tab(title: "Tab 1", viewModel: TerminalViewModel())]
+            }
+        }
+    }
+
+    func observeTabChanges() {
+        cancellables.removeAll()
+        for tab in tabs {
+            tab.$currentDirectory
+                .debounce(for: .milliseconds(100), scheduler: DispatchQueue.main)
+                .sink { [weak self] _ in
+                    self?.saveTabs(self?.tabs ?? [])
+                }
+                .store(in: &cancellables)
+        }
+    }
+
+    func saveTabs(_ tabs: [Tab]) {
+        let encoder = JSONEncoder()
+        encoder.outputFormatting = .prettyPrinted // Enable pretty printing
+        do {
+            let data = try encoder.encode(tabs)
+            if FileManager.default.fileExists(atPath: databaseFileUrl.path) {
+                try FileManager.default.removeItem(at: databaseFileUrl)
+            }
+            try data.write(to: databaseFileUrl)
+        } catch {
+            print("Failed to save tabs: \(error)")
+        }
+    }
+}
---
+++ moodterm/Views/ContentView.swift
@@ -0,0 +1,21 @@
+import SwiftUI
+
+/// The main view of the application.
+struct ContentView: View {
+    @EnvironmentObject var tabStore: TabStore
+    @EnvironmentObject var userSettings: UserSettings
+
+    var body: some View {
+        TabView(
+            tabs: $tabStore.tabs, selectedTab: $tabStore.selectedTab,
+            fontSizeFactor: $userSettings.fontSizeFactor
+        )
+        .onAppear {
+            tabStore.selectedTab = tabStore.tabs.first?.id
+            tabStore.observeTabChanges()
+        }
+        .onChange(of: tabStore.tabs) {
+            tabStore.observeTabChanges()
+        }
+    }
+}
